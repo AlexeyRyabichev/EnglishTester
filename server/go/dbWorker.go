893854e18428027934ce5f424692d9b0a0376a52
Alexey Ryabichev <alexey.ryabichev@gmail.com>
2019-02-17 12:50:51 +0300

    Server init

---
+++ server/go/dbWorker.go
@@ -0,0 +1,34 @@
+package swagger
+
+import (
+	"fmt"
+	"github.com/go-pg/pg"
+	"github.com/go-pg/pg/orm"
+)
+
+var db *pg.DB
+
+func InitDB()  {
+	db = pg.Connect(&pg.Options{
+		User:"postgres",
+		Password:"tigra",
+	})
+	err := createSchema(db)
+	if(err==nil){
+		fmt.Print("kk")
+	}
+
+	
+}
+
+func createSchema(db *pg.DB) error {
+	for _, model := range []interface{}{(*Student)(nil), (*Test)(nil)} {
+		err := db.CreateTable(model, &orm.CreateTableOptions{
+			Temp: false,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
