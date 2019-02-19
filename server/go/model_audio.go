99719ab99d8870b657d59d26989cb848f1132495
MTigra <tomartiroyan@edu.hse.ru>
2019-02-19 17:00:45 +0300

    Moving API from / to /api/

--- server/go/model_audio.go
+++ server/go/model_audio.go
@@ -1,7 +1,7 @@
 package swagger
 
 type Audio struct {
-	Id int `json:"id,omitempty"`
-	Student Student `json:"student,omitempty"`
+	Id int64 `json:"id,omitempty"`
+	StudentId int64 `json:"student,omitempty"`
 	Path string `json:"path,omitempty"`
 }
