9d7fba82f1ffac82843d6d0a708215938f4b0230
Alexey Ryabichev <alexey.ryabichev@gmail.com>
2019-02-22 23:21:48 +0300

    All pages initialized

--- web/jsPrivate/router.js
+++ web/jsPrivate/router.js
@@ -23,26 +23,60 @@ function open(path, response){
     })
 }
 
+function parseCookies(request) {
+    var list = {}, rc = request.headers.cookie;
+
+    rc && rc.split(';').forEach(function (cookie) {
+        var parts = cookie.split('=');
+        list[parts.shift().trim()] = decodeURI(parts.join('='));
+    });
+
+    return list;
+}
+
+function handleAuth() {
+    return true;
+}
+
+function getSessionID() {
+
+}
+
 module.exports = {
     init: function (request, response) {
         console.log(`Requested: ${request.url}`);
-
+        console.log(parseCookies(request));
         switch (request.url) {
-            case "/students.html":
-                let data = [];
-                request.on('data', chunk => {
-                    data.push(chunk);
-                });
-                request.on('end', () => {
-                    console.log(fs.readAsText(data));
-                });
+            case "/auth":
+                if (handleAuth()){
+                    request.url = "./teacher/students.html";
+                    response.writeHead(200, {
+                        'Set-Cookie': 'id=' + new Date().getMilliseconds()
+                    });
+                }
+                else
+                    request.url = "./teacher/index.html";
                 break;
             case "/":
                 request.url = "./teacher/index.html";
                 break;
+            case "/index.html":
+                request.url = "./teacher/index.html";
+                break;
+            case "/students.html":
+                request.url = "./teacher/students.html";
+                break;
+            case "/tests.html":
+                request.url = "./teacher/tests.html";
+                break;
+            case "/results.html":
+                request.url = "./teacher/results.html";
+                break;
+            case "/settings.html":
+                request.url = "./teacher/settings.html";
+                break;
             default:
-                request.url = "." + request.url;
-                // request.url = path.join(__dirname, request.url);
+                request.url = path.join(__dirname, "../", request.url);
                 break;
         }
         open(request.url, response);
